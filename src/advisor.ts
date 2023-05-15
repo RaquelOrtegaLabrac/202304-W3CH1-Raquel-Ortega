a0221609865885e14ece0f947e7181c547b1e8c4
labrac <raquelortegalabrac@hotmail.com>
2023-05-15 20:18:53 +0200

    Add index.ts

---
+++ src/advisor.ts
@@ -0,0 +1,15 @@
+/* eslint-disable max-params */
+import { Character } from './characters.js';
+
+export class Advisor extends Character {
+  constructor(
+    name: string,
+    house: string,
+    age: number,
+    advisedCharacter: string
+  ) {
+    super(name, house, age);
+    this.advisedCharacter = advisedCharacter;
+    this.speech = 'No sé por qué, pero creo que voy a morir pronto';
+  }
+}
